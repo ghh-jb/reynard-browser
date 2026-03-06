6bb337f00389faae8e6a58521a55326b6d24a9de
Minh Ton <37860569+minh-ton@users.noreply.github.com>
2026-03-06 22:29:39 +0700

    Mark app version as alpha; add github actions build workflow; update build script to use dynamic build string based on date

--- tools/release/build-app.sh
+++ tools/release/build-app.sh
@@ -13,7 +13,12 @@ cd "$ROOT_DIR"
 "$ROOT_DIR/tools/development/build-gecko.sh"
 
 rm -rf "$DIST_DIR"
+mkdir -p "$DIST_DIR"
+
+TODAY=$(date +%Y%b%d | tr '[:lower:]' '[:upper:]')
+cp "$XCCONFIG_PATH" "$DIST_DIR/Reynard.xcconfig"
+sed -i '' "s/CURRENT_BUILD = .*/CURRENT_BUILD = $TODAY/" "$DIST_DIR/Reynard.xcconfig"
 
 xcodebuild clean -scheme "Reynard" -project "$PROJECT_PATH" -sdk iphoneos -arch arm64 -configuration Release
 
-xcodebuild archive -scheme "Reynard" -archivePath "$DIST_DIR/Reynard.xcarchive" -project "$PROJECT_PATH" -sdk iphoneos -arch arm64 -configuration Release -xcconfig "$XCCONFIG_PATH"
+xcodebuild archive -scheme "Reynard" -archivePath "$DIST_DIR/Reynard.xcarchive" -project "$PROJECT_PATH" -sdk iphoneos -arch arm64 -configuration Release -xcconfig "$DIST_DIR/Reynard.xcconfig"

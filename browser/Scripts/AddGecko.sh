2837682a7ed08365b9da902fb32ca2187ba56659
Minh Ton <37860569+minh-ton@users.noreply.github.com>
2026-04-16 01:21:38 +0700

    Update the Gecko copy script to fix lots of missing scripts error

--- browser/Scripts/AddGecko.sh
+++ browser/Scripts/AddGecko.sh
@@ -7,38 +7,30 @@ APP_BUNDLE="${TARGET_BUILD_DIR}/${WRAPPER_NAME}"
 FRAMEWORKS_DIR="${APP_BUNDLE}/Frameworks"
 GECKOVIEW_FW="${FRAMEWORKS_DIR}/GeckoView.framework"
 GECKOVIEW_FW_FRAMEWORKS="${GECKOVIEW_FW}/Frameworks"
+
 SIGN_IDENTITY="${EXPANDED_CODE_SIGN_IDENTITY:-${EXPANDED_CODE_SIGN_IDENTITY_NAME:-Apple Development}}"
 DEFAULT_THEME_SRC="${SRCROOT}/../engine/firefox/toolkit/mozapps/extensions/default-theme"
 
 mkdir -p "${FRAMEWORKS_DIR}"
+mkdir -p "${GECKOVIEW_FW_FRAMEWORKS}"
 
+# copy dylibs and XUL, then sign
 cp -fL "${GECKO_DIST_BIN}/"*.dylib "${FRAMEWORKS_DIR}/"
-cp -fL "${GECKO_DIST_BIN}/plugin-container" "${FRAMEWORKS_DIR}/"
+cp -fL "${GECKO_DIST_BIN}/XUL" "${GECKOVIEW_FW}/XUL"
 
-for lib in "${FRAMEWORKS_DIR}/XUL" "${FRAMEWORKS_DIR}/plugin-container" "${FRAMEWORKS_DIR}/"*.dylib; do
-	if [ -f "${lib}" ]; then
-		codesign --force --sign "${SIGN_IDENTITY}" "${lib}"
+for file in "${GECKOVIEW_FW}/XUL" "${FRAMEWORKS_DIR}/"*.dylib; do
+	if [ -f "${file}" ]; then
+		codesign --force --sign "${SIGN_IDENTITY}" --preserve-metadata=identifier,entitlements,resource-rules "${file}"
 	fi
 done
 
-mkdir -p "${GECKOVIEW_FW_FRAMEWORKS}"
+# copy the rest of the files, excluding the ones we already copied and the test files
+rsync -pvtrlL --delete --exclude "XUL" --exclude "*.dylib" --exclude "Test*" --exclude "test_*" --exclude "*_unittest" "${GECKO_DIST_BIN}/" "${GECKOVIEW_FW_FRAMEWORKS}"
 
-cp -fL "${GECKO_DIST_BIN}/XUL" "${GECKOVIEW_FW}/XUL"
-codesign --force --sign "${SIGN_IDENTITY}" "${GECKOVIEW_FW}/XUL"
-
-cp -fL "${GECKO_DIST_BIN}/plugin-container" "${GECKOVIEW_FW_FRAMEWORKS}/"
-codesign --force --sign "${SIGN_IDENTITY}" "${GECKOVIEW_FW_FRAMEWORKS}/plugin-container"
-
-for resource in greprefs.js application.ini platform.ini chrome.manifest dependentlibs.list; do
-	cp -fL "${GECKO_DIST_BIN}/${resource}" "${GECKOVIEW_FW_FRAMEWORKS}/"
-done
-
-for directory in chrome defaults res components modules actors localization contentaccessible hyphenation dictionaries; do
-	cp -RfL "${GECKO_DIST_BIN}/${directory}" "${GECKOVIEW_FW_FRAMEWORKS}/"
-done
-
+# default theme missing error fix
 mkdir -p "${GECKOVIEW_FW_FRAMEWORKS}/default-theme"
 cp -RfL "${DEFAULT_THEME_SRC}/" "${GECKOVIEW_FW_FRAMEWORKS}/default-theme/"
 echo "resource default-theme file:default-theme/" >> "${GECKOVIEW_FW_FRAMEWORKS}/chrome.manifest"
 
+# sign the GeckoView.framework
 codesign --force --sign "${SIGN_IDENTITY}" "${GECKOVIEW_FW}"
